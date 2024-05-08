dc446fe8dcf78a31e54f824e5991ce427da0f4f2
Filip Rojek <filip@filiprojek.cz>
2024-05-08 16:37:48 +0200

    Clean up

--- frontend/app/_layout.js
+++ frontend/app/_layout.js
@@ -1,9 +1,11 @@
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { AuthProvider } from "./context/AuthContext";
 
 export default function Root() {
   return (
     <AuthProvider>
+      <StatusBar style="light" />
       <Slot />
     </AuthProvider>
   );
